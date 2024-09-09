9a6bab16cf125c64be3c70568b6b143615a76130
Amanuel Sisay <amanuelsisay.as@gmail.com>
2024-09-09 17:30:59 +0300

    (refactor): cleaning ups some unnecessary code

--- src/shoe/shoe.module.ts
+++ src/shoe/shoe.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ShoeService } from './shoe.service';
-import { ShoeController } from './shoe.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Shoe } from './entities/shoe.entity';
 import { Cart, CartItem } from 'src/carts/entities/cart.entity';
@@ -8,7 +7,7 @@ import { CartsService } from 'src/carts/carts.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Shoe, Cart, CartItem])],
-  controllers: [ShoeController],
+  controllers: [],
   providers: [ShoeService, CartsService],
 })
 export class ShoeModule {}
