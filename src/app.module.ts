122c6f79660af036dced3c0cd3a290c6f81d7934
Amanuel Sisay <amanuelsisay.as@gmail.com>
2024-09-09 12:26:28 +0300

    (fixed): removed unnecesary packages, and imports from here

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 import { ShoeModule } from './shoe/shoe.module';
 import { CartsModule } from './carts/carts.module';
 import { Shoe } from './shoe/entities/shoe.entity';
 import { Cart, CartItem } from './carts/entities/cart.entity';
-import { User } from './users/entities/user.entity';
+// import { User } from './users/entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GeminiService } from './gemini/gemini.service';
 import { GeminiController } from './gemini/gemini.controller';
 import { GeminiModule } from './gemini/gemini.module';
-import { AuthModule } from './auth/auth.module';
+// import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, ShoeModule, CartsModule, GeminiModule,
+  imports: [
+    // UsersModule,
+     ShoeModule, CartsModule, GeminiModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.MYSQL_HOST,
@@ -22,7 +23,9 @@ import { AuthModule } from './auth/auth.module';
       username: process.env.MYSQL_USER,
       password: process.env.MYSQL_PASSWORD,
       database: process.env.MYSQL_DATABASE,
-      entities: [Shoe, Cart, CartItem, User ],
+      entities: [Shoe, Cart, CartItem, 
+        // User
+      ],
       synchronize: true, // Set to false in production and use migrations
     }),
     // AuthModule,
